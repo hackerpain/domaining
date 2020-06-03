9e772513cb64a0d6e6d601130dd1ea40094f0529
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-06-03 21:05:57 +0530

    Add Rate Limit Bypass to optimize
    Add Rate Limit Bypass to optimize & make more queries w/o login

--- govalue.sh
+++ govalue.sh
@@ -10,10 +10,16 @@ value () {
 # Find value of individual domains with value ()
 
 bulk_value () {
+              declare -i wait=10
+              declare -i ctr=1
               while read dom; do
-                echo -e "The value of ${dom} is USD $(curl -s "https://api.godaddy.com/v1/appraisal/${dom}" | jq '.govalue') \n"
-                sleep $2
-              done < $1
+                data=$(curl -s "https://api.godaddy.com/v1/appraisal/${dom}")
+                if [ "$(echo $data | jq '.message')" != "null" ]; then
+                    echo -e "\nWe hit the rate limit, so slowing down for $(( wait*ctr ))s\n"; sleep $(( wait * ctr )); echo -e "\nStarting bulk search, if its still unreliable, end the process, wait for longer, then continue\n"; ((ctr++));
+                else
+                    echo -e "The value of ${dom} is USD $(echo $data | jq '.govalue') \n"; sleep $2;
+                fi
+                done < $1
 }
 
 # Find value from a list of domains (.txt) file, pasted one after the other line by line (delimited by newline)
