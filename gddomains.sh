360541a1d9f02bdc399858911926ef0dd1b819e8
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-06-05 21:56:17 +0530

    Update gddomains.sh

--- gddomains.sh
+++ gddomains.sh
@@ -1,5 +1,5 @@
 gd_domains () {
-  curl -s "https:/api.ote-godaddy.com/v1/domains" -H "Accept: application/json" -H "Authorization: sso-key $GDkey:$GDsecret" | jq -r '.[].domain' > $1
+  curl -s "https:/api.godaddy.com/v1/domains" -H "Accept: application/json" -H "Authorization: sso-key $GDkey:$GDsecret" | jq -r '.[].domain' > $1
 }
 
-# Usage - gd_domains ouput_file_name.txt
+# Usage - gd_domains output_file_name.txt
