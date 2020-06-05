d2641085bea31fefca0095b4c453e1ac751a6df6
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-06-05 21:42:38 +0530

    valuations saved to filename-value.txt & auth integrated to increase rate limit
    improved

--- govalue.sh
+++ govalue.sh
@@ -13,11 +13,11 @@ bulk_value () {
               declare -i wait=10
               declare -i ctr=1
               while read dom; do
-                data=$(curl -s "https://api.godaddy.com/v1/appraisal/${dom}")
+                data=$(curl -s "https://api.godaddy.com/v1/appraisal/${dom}" -H "Authorization: sso-key $GDkey:$GDsecret")
                 if [ "$(echo $data | jq '.message')" != "null" ]; then
                     echo -e "\nWe hit the rate limit, so slowing down for $(( wait*ctr ))s\n"; sleep $(( wait * ctr )); echo -e "\nStarting bulk search, if its still unreliable, end the process, wait for longer, then continue\n"; ((ctr++));
                 else
-                    echo -e "The value of ${dom} is USD $(echo $data | jq '.govalue') \n"; sleep $2;
+                    echo -e "The value of ${dom} is USD $(echo $data | jq '.govalue') \n" > $(echo $1 | cut -d '.' -f 1)-value.txt; sleep $2;
                 fi
                 done < $1
 }
