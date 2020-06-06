bb978852e5601c3c1237238c72fd47231262e78d
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-06-06 11:53:11 +0530

    Update bulklogo.sh

--- bulklogo.sh
+++ bulklogo.sh
@@ -1,7 +1,9 @@
 bulk_logo () {
      # Optional - gd_domains $1
+     echo -e "\nGenerating Logos, please be patient as the process takes some time\n"
      while read dom; do
         name=$(echo $dom | cut -d '.' -f 1)
+        echo -e "Generating Logo fo $dom, with the text $name\n"
         curl -s "https://api-cdn.logolava.com/v1/?margin=.15&w=600&h=300&&randicon=0.80&style=fancy&api_key=$LLkey&txt=$name" > $name.png # without API key workaround: https://api-cdn.logolava.com/v1/?homepage=1&txt=$name
         sleep $2
      done < $1
